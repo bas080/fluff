8178d6ee767229e14342d971ed7c001154d13bc4
Bas Huis <bas080@hotmail.com>
2018-06-11 06:02:44 -0400

    Create streams outside of fluss stream make fn
    This changes the api

--- index.js
+++ index.js
@@ -3,12 +3,22 @@
 import tmp from 'tmp'
 import streamToPromise from 'stream-to-promise'
 import fluffStream from './lib/index.js'
+import {createReadStream, createWriteStream} from 'fs'
 
 tmp.file((err, path, _, cleanup) => {
   if (err)
     throw err
 
-  streamToPromise(fluffStream(process.stdin, path).pipe(process.stdout))
+  process.stderr.write(`fluff: ${path}\n`)
+  process.stdin.on('end', () => {
+    process.stderr.write(`fluff: done buffering to ${path} \n`)
+  })
+
+  streamToPromise(fluffStream({
+    inStream: process.stdin,
+    createReadStream: () => createReadStream(path),
+    createWriteStream: () => createWriteStream(path, {start: process.stdout.bytesWritten})
+  }).pipe(process.stdout))
     .then(cleanup)
     .catch(error => process.stderr.write(`fluff: ${error}\n`))
 })
